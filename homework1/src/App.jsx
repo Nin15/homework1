d322969a85d153a1667b5ec9a5186996276da2ac
nino shubitidze <ninomqvia@gmail.com>
2024-09-28 11:01:34 +0400

    first commit

---
+++ homework1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css"; 
+
+function App() {
+  return (
+    <div>
+      <h1>Nino Shubitidze</h1>
+      <h1>:)</h1>
+      <h1>16 Years Old</h1>
+      <h1>I like drawing</h1>
+    </div>
+  );
+}
+
+export default App;
